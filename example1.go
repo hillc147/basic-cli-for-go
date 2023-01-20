8798b5114fe8c5596ade85d47c6218db30fe6486
Christian Hill <120223382+hillc147@users.noreply.github.com>
2023-01-20 12:33:24 -0600

    Add files via upload

---
+++ example1.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const DaysTotal int = 90
+	challenge := "#90DaysOfDevOps"
+	var remaining uint = 90
+
+	fmt.Printf("Welcome to the %v challenge.\nThis challenge consists of %v days\n", challenge, DaysTotal)
+
+	var TwitterName string
+	var DaysCompleted uint
+
+	// asking for user input
+	fmt.Println("Enter Your Twitter Handle: ")
+	fmt.Scanln(&TwitterName)
+
+	fmt.Println("How many days have you completed?: ")
+	fmt.Scanln(&DaysCompleted)
+
+	remaining = remaining - DaysCompleted
+
+	fmt.Printf("Thank you %v for taking part and completing %v days.\n", TwitterName, DaysCompleted)
+	fmt.Println("Good luck")
+	fmt.Printf("%v days left", remaining)
+}
